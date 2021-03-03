ad0582c4cc301c24ebf8301123d1f1f44ec48f4c
zhizhoualan <alan.zhizhou@gmail.com>
2021-03-02 22:59:49 -0800

    Enable Chinese version

--- superset/config.py
+++ superset/config.py
@@ -287,8 +287,10 @@ LANGUAGES = {
 }
 # Turning off i18n by default as translation in most languages are
 # incomplete and not well maintained.
-LANGUAGES = {}
-
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    "zh": {"flag": "cn", "name": "Chinese"},
+}
 # ---------------------------------------------------
 # Feature flags
 # ---------------------------------------------------
